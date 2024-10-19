fd7d0128300e1b8ca731b97ac3b0acef6887e241
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2024-10-20 01:58:39 +0300

    initial commit

--- app/Livewire/Pages/Contact.php
+++ app/Livewire/Pages/Contact.php
@@ -6,6 +6,27 @@ use Livewire\Component;
 
 class Contact extends Component
 {
+    public $name ='';
+    public $email = '';
+    public $subject = '';
+    public $message = '';
+
+    protected $rules = [
+        'name'=>'required|min:6',
+        'email' => 'required|email',
+        'subject' => 'required',
+        'message' => 'required'
+    ];
+
+    public function Updated($propertyName){
+        $this->validateOnly($propertyName);
+    }
+
+    public function send(){
+        Session()->flash('success','Message sent');
+        $this->reset();
+    }
+
     public function render()
     {
         return view('livewire.pages.contact');
--- resources/views/livewire/pages/contact.blade.php
+++ resources/views/livewire/pages/contact.blade.php
@@ -30,23 +30,23 @@
                     <a class="contact-btns" href="#">Create New Account</a>
                 </div>
                 <div class="col-lg-8">
-                    <form class="form-area " id="myForm" action="mail.php" method="post"
+                    <form class="form-area " id="myForm" action="mail.php" wire:submit=send method="post"
                         class="contact-form text-right">
                         <div class="row">
                             <div class="col-lg-12 form-group">
-                                <input name="name" placeholder="Enter your name" onfocus="this.placeholder = ''"
+                                <input  wire:model="name" placeholder="Enter your name" onfocus="this.placeholder = ''"
                                     onblur="this.placeholder = 'Enter your name'"
                                     class="common-input mb-20 form-control" required="" type="text">
 
-                                <input name="email" placeholder="Enter email address"
+                                <input wire:model="email" placeholder="Enter email address"
                                     pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
                                     onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'"
                                     class="common-input mb-20 form-control" required="" type="email">
 
-                                <input name="subject" placeholder="Enter your subject" onfocus="this.placeholder = ''"
+                                <input wire:model="subject" placeholder="Enter your subject" onfocus="this.placeholder = ''"
                                     onblur="this.placeholder = 'Enter your subject'"
                                     class="common-input mb-20 form-control" required="" type="text">
-                                <textarea class="common-textarea mt-10 form-control" name="message"
+                                <textarea class="common-textarea mt-10 form-control" wire:model="message"
                                     placeholder="Messege" onfocus="this.placeholder = ''"
                                     onblur="this.placeholder = 'Messege'" required=""></textarea>
                                 <button class="primary-btn mt-20 text-white" style="float: right;">Send Message</button>
